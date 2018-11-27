47d111fe5d307780eec200e11cbe7cf562a579d4
coult <felipemkautzmann@gmail.com>
2018-11-27 18:14:52 +0100

    This is just the beginning of everything
    We are just trying the basic steps from git

--- webscraping1.py
+++ webscraping1.py
@@ -3,12 +3,12 @@ r = requests.get("http://example.webscraping.com")
 country = " Afghanistan"
 position = r.text.find(country)
 #finding the closest <a href=" through rfind
-afghanlink = r.text.rfind('href="',0,position)
+afghanlink = r.text.rfind('href="', 0, position)
 afghanlink += 6
 
 #finding the ending of the href bracket
 
-afghanend = r.text.find('"',afghanlink)
+afghanend = r.text.find('"', afghanlink)
 web = "http://example.webscraping.com" + r.text[afghanlink:afghanend]
 q = requests.get(web)
 print(q.text)
